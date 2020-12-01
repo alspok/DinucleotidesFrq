e4f3695110a4339ada7d99ce7a9a8dcc8e154769
alvydas <alspok@gmail.com>
2020-12-02 01:31:21 +0200

    .

--- DinucleotidesFrq/Helpers/Match.cs
+++ DinucleotidesFrq/Helpers/Match.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+
 namespace DinucleotidesFrq.Helpers
 {
     public class Match
     {
-        public Match()
+        public bool MatchSeq(string seq)
         {
+            return Regex.IsMatch(seq, @"^[a-zA-Z0-9]*$");
         }
     }
 }

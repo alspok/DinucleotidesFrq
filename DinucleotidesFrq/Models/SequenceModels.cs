5c24fe047b25b0725700556a3067f63d8999bbb3
alvydas <alspok@gmail.com>
2020-12-14 01:31:33 +0200

    .

--- DinucleotidesFrq/Models/SequenceModels.cs
+++ DinucleotidesFrq/Models/SequenceModels.cs
@@ -1,40 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace DinucleotidesFrq.Models
 {
 
     public class SequenceModels
     {
-        public TestSequenceModel TestSeq {get;set;}
-        public RandomSequenceModel RandomSeq { get; set; }
-        public SelectSequenceModel SelectSeq { get; set; }
-        public GBSequenceModel GBSeq { get; set; }
+        public InputSequence InputSeq { get; set; }
+        public TestSequence TestSeq {get;set;}
+        public RandomSequence RandomSeq { get; set; }
+        public SelectSequence SelectSeq { get; set; }
+        public GBSequence GBSeq { get; set; }
     }
 
-    public class TestSequenceModel
+    public class InputSequence
     {
         public string Seq { get; set; }
-        public string SeqNames { get; set; }
+        public string SeqName { get; set; }
         public int SeqLength { get; set; }
     }
 
-    public class RandomSequenceModel
+    public class TestSequence
+    {
+        public class TestSeq
+        {
+            public string Seq { get; set; }
+            public string SeqName { get; set; }
+            public int SeqLength { get; set; }
+        }
+
+        public List<TestSeq> TSeq { get; set; } = new List<TestSeq>
+        {
+            new TestSeq { Seq = "aaacagatcacccgctgagcgggttatctgtt", SeqName = "some", SeqLength = 32 },
+            new TestSeq { Seq = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", SeqName = "", SeqLength = 32 },
+            new TestSeq { Seq = "agcgggctagtgcgtgagcgggctagtgcgtg", SeqName = "", SeqLength = 32},
+            new TestSeq { Seq = "agcgggctagtgcgtgcagcgggctagtgcgtg", SeqName = "", SeqLength = 33}
+        };
+    }
+
+    public class RandomSequence
     {
         public string Seq { get; set; }
-        public string SeqNames { get; set; }
+        public string SeqName { get; set; }
         public int SeqLength { get; set; }
     }
 
-    public class SelectSequenceModel
+    public class SelectSequence
     {
         public string Seq { get; set; }
-        public string SeqNames { get; set; }
+        public List<string> SeqName { get; set; } = new List<string>
+        {
+            "Escherichia coli",
+            "Lactococcus lactis",
+            "Someone some"
+        };
         public int SeqLength { get; set; }
     }
 
-    public class GBSequenceModel
+    public class GBSequence
     {
         public string Seq { get; set; }
-        public string SeqNames { get; set; }
+        public string SeqName { get; set; }
         public int SeqLength { get; set; }
     }
 }
